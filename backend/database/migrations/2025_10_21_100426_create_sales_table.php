5b2f6ec7ee8cfff1f9d6c7cec4abd9fc118630e3
Dylaurette YOUKOU <dylaurette87@gmail.com>
2025-10-21 15:16:46 +0000

    erp

--- backend/database/migrations/2025_10_21_100426_create_sales_table.php
+++ backend/database/migrations/2025_10_21_100426_create_sales_table.php
@@ -4,24 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateSalesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->string('invoice_number')->unique()->nullable();
+            $table->foreignId('site_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete(); // vendeur
+            $table->string('client_name')->nullable();
+            $table->string('client_email')->nullable();
+            $table->decimal('sub_total', 12, 2)->default(0);
+            $table->decimal('tax', 12, 2)->default(0);
+            $table->decimal('discount', 12, 2)->default(0);
+            $table->decimal('total', 12, 2)->default(0);
+            $table->enum('status', ['draft','finalized','cancelled'])->default('draft');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('sales');
     }
-};
+}
