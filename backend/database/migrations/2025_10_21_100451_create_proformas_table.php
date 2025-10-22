e0ffc60101fd1b285c3b0ba896602ce8faf93c0c
Dylaurette YOUKOU <dylaurette87@gmail.com>
2025-10-22 10:30:12 +0000

    erp

--- backend/database/migrations/2025_10_21_100451_create_proformas_table.php
+++ backend/database/migrations/2025_10_21_100451_create_proformas_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProformasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('proformas', function (Blueprint $table) {
             $table->id();
+            $table->string('reference')->unique();
+            $table->foreignId('site_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete(); // technicien
+            $table->text('description')->nullable();
+            $table->decimal('total', 12, 2)->default(0);
+            $table->enum('status', ['draft','sent','accepted','rejected'])->default('draft');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('proformas');
     }
-};
+}
