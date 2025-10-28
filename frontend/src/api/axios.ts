ccffd6c8e4c8ce2d461fd8b1c0843de5c69a4349
Dylaurette YOUKOU <dylaurette87@gmail.com>
2025-10-28 16:09:32 +0000

    tailwind

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/api', // backend Laravel
   withCredentials: true, // important pour Sanctum (cookies, sessions)
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
